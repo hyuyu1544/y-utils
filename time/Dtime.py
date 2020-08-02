c700f9eaea2b9c30ab73a7d8d0cf7e59f37bcd26
Yu-Yu Huang <hyuyu1544@gmail.com>
2020-08-02 21:40:51 +0800

    new repository

---
+++ time/Dtime.py
@@ -0,0 +1,24 @@
+from functools import wraps
+
+import time
+
+
+# TODO:: 打包給pip install可用
+# TODO:: 確定好結構，及每個func名稱，說明文件
+
+
+def Count_timer(func):
+    """Decorator for counting timer."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        pass
+
+
+def Timming_timer(func):
+    """Decorator for timming timer."""
+    pass
+
+
+def Retry_timer(func):
+    """Decorator for retry func."""
+    pass
